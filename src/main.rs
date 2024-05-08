5d17b005f9fb7e7ca6d3e631f94acbd9047102f4
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-05-08 22:56:30 +0300

    Now rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+use std::{fs::read_to_string, path::Path};
+
+use suoi_game::player::Player;
+
+use suoi_rwin::{
+    shader::ShaderStage, Camera, Context, EventHandler, GLFWContext, Model, Mouse, Renderer,
+    Screen, ShaderStageType, Time,
+};
+use suoi_simp::{obj::Obj, Resource};
+use suoi_types::{Color, Matrix, Matrix4, Vector3};
+
+const CLEAR_COLOR: Color = Color::rgb(31, 31, 31);
+
 fn main() {
-    println!("Hello, world!");
+    let mut camera = Camera::default();
+    let mut player = Player::default();
+
+    let mut event_handler = EventHandler::default();
+    let mut screen = Screen::new(800, 480);
+    let mut context = Context::init(&screen);
+
+    let mut time = Time::default();
+    let mut mouse = Mouse::default();
+
+    let vert_data = &read_to_string("assets/shaders/normal_map.vert").unwrap();
+    let frag_data = &read_to_string("assets/shaders/normal_map.frag").unwrap();
+
+    let shader = unsafe {
+        suoi_rwin::Shader::compile(
+            ShaderStage::compile(vert_data, ShaderStageType::Vertex).unwrap(),
+            ShaderStage::compile(frag_data, ShaderStageType::Fragment).unwrap(),
+        )
+    }
+    .unwrap();
+
+    let model_path = Path::new("assets/models/stuff.obj");
+    let obj = Obj::import(model_path).expect("IMPORT_ERROR");
+
+    let stuff_model = Model::from(obj);
+
+    unsafe { Renderer::init() };
+
+    while context.running() {
+        context.window_mut().swap_buffers();
+        unsafe {
+            Renderer::clear_screen(CLEAR_COLOR);
+            shader.with(|| {
+                shader.set_uniform("texture1", 1);
+
+                // Transformation Matrices
+                let model = Matrix4::translate(Vector3::fwd() * -5.0);
+                let view = camera.view_matrix();
+                let projection = camera.projection_matrix(&screen);
+
+                // set uniform matrices
+                shader.set_uniform("model", model.transposition());
+                shader.set_uniform("view", view);
+                shader.set_uniform("projection", projection.transposition());
+
+                stuff_model.draw();
+            });
+        }
+
+        // poll systems
+        time.poll(&context);
+        mouse.poll_delta();
+        event_handler.poll_events(&mut context, &mut screen, &mut mouse);
+
+        // update
+        player.update(&mut context, time.delta(), &mouse, &mut camera);
+    }
 }
---
+++ src/player.rs
@@ -0,0 +1,70 @@
+use suoi_rwin::{Camera, Context, Key, Keyboard, Mouse};
+use suoi_types::{Deg, Quaternion, Vector2, Vector3};
+
+pub struct Player {
+    sensitivity: f32,
+
+    pitch: f32,
+    yaw: f32,
+}
+
+impl Default for Player {
+    fn default() -> Self {
+        Self {
+            sensitivity: 5.0,
+            pitch: Default::default(),
+            yaw: Default::default(),
+        }
+    }
+}
+
+impl Player {
+    pub fn update(
+        &mut self,
+        context: &mut Context,
+        delta_time: f32,
+        mouse: &Mouse,
+        camera: &mut Camera,
+    ) {
+        if Keyboard::get_key(Key::Esc, context).is_pressed() {
+            context.close()
+        }
+    
+        self.move_self(context, delta_time, camera);
+        self.turn_camera(delta_time, mouse, camera);
+    }
+
+    fn move_self(&mut self, context: &mut Context, delta_time: f32, camera: &mut Camera) {
+        let move_axes = Vector2 {
+            x: Keyboard::input_axis(&context, Key::A, Key::D),
+            y: Keyboard::input_axis(&context, Key::S, Key::W),
+        };
+
+        let fwd = camera.transform.forward();
+        let right = camera.transform.right();
+
+        let speed = 5.0;
+
+        camera
+            .transform
+            .translate((fwd * move_axes.y - right * move_axes.x) * speed * delta_time);
+    }
+
+    #[rustfmt::skip]
+    fn turn_camera(&mut self, delta_time: f32, mouse: &Mouse, camera: &mut Camera) {
+        self.yaw -= mouse.delta().x * self.sensitivity * delta_time;
+        self.pitch -= mouse.delta().y * self.sensitivity * delta_time;
+
+        if self.pitch > 89.0 {
+            self.pitch = 89.0;
+        }
+        if self.pitch < -89.0 {
+            self.pitch = -89.0;
+        }
+
+        camera.transform.set_rotation(
+            Quaternion::axis_angle(Vector3::up(), Deg(self.yaw)) *
+            Quaternion::axis_angle(Vector3::right(), Deg(self.pitch)),
+        );
+    }
+}
