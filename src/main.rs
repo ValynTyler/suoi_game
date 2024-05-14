436152b93748548afad897d4437e68cccf3465df
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-05-14 07:00:07 +0300

    Update submodules

--- src/main.rs
+++ src/main.rs
@@ -79,13 +79,13 @@ fn main() {
                 shader.set_uniform("texture1", 1);
 
                 // set uniform matrices
-                shader.set_uniform("view", camera.view_matrix());
+                shader.set_uniform("view", &camera.view_matrix());
                 shader.set_uniform(
                     "projection",
-                    camera.projection_matrix(&screen).transposition(),
+                    &camera.projection_matrix(&screen).transpose(),
                 );
 
-                shader.set_uniform("model", Matrix4::identity());
+                shader.set_uniform("model", &Matrix4::identity());
                 model.draw();
             });
 
@@ -94,13 +94,13 @@ fn main() {
                 ui_shader.set_uniform("texture1", 1);
 
                 // set uniform matrices
-                ui_shader.set_uniform("view", ui_cam.view_matrix());
+                ui_shader.set_uniform("view", &ui_cam.view_matrix());
                 ui_shader.set_uniform(
                     "projection",
-                    ui_cam.projection_matrix(&screen).transposition(),
+                    &ui_cam.projection_matrix(&screen).transpose(),
                 );
 
-                ui_shader.set_uniform("model", Matrix4::uniform_scale(5.0));
+                ui_shader.set_uniform("model", &Matrix4::uniform_scale(5.0));
                 quad.draw();
             });
         }
